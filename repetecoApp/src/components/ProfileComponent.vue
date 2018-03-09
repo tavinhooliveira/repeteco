53a651fb704d6205d4435b1993cddbd9d758a654
Luis Oliveira <luis.o.oliveira@accenture.com>
2018-03-09 00:39:44 -0300

    Ajustes modal compatibilidade com o css, js do progressBar Circulos

--- repetecoApp/src/components/ProfileComponent.vue
+++ repetecoApp/src/components/ProfileComponent.vue
@@ -7,12 +7,13 @@
             </div>
             <div>
                <h4 class="perfilName center-block"><a v-bind:href="link" target="_blank">{{name}}</a>
+               <h6 class="small">{{friendsTotalApp}} de {{friendsTotalFb}} <i class="fa fa-users"></i> </h6>
                </h4>
             </div>
             
-            <!--
+           <!-- 
             <div class="row perfilbrprogress">
-               <p class="pull-right" id="friendsCount">{{friendsTotalApp}} de {{friendsTotalFb}} <i class="fa fa-users"></i> </p>
+               <p class="" id="friendsCount">{{friendsTotalApp}} de {{friendsTotalFb}} <i class="fa fa-users"></i> </p>
                <div class="progress progress-striped ">
                   <div class="bar" role="progressbar" :style="'height:30px;background:#1E8BC3;width:' + friendsTotalApp + '%'"><b class="text-secondary">{{progress()}}%</b></div>
                </div>
@@ -45,7 +46,7 @@
             <div>
                 <div class="center-block well progressBarCirculos">
                     <div class="row center-block centroDiv">
-                        <div class="col-md-3 col-sm-6 center-block">
+                        <div class="col-md-3  center-block" v-tooltip.top-start="'Amigos no Aplicativo'">
                             <div class="progress blue">
                                 <span class="progress-left">
                                     <span class="progress-bar"></span>
@@ -53,18 +54,29 @@
                                 <span class="progress-right">
                                     <span class="progress-bar"></span>
                                 </span>
-                                <div class="progress-value">{{friendsTotalApp}} APP</div>
+                                <div class="progress-value">{{friendsTotalApp}} </div>
                             </div>
                         </div>
-                        <div class="col-md-3 col-sm-4 center-block " >
-                            <div class="progress yellow">
+                        <div class="col-md-3  center-block" v-tooltip.top-start="'Amigos no Facebook'" >
+                            <div class="progress yellow">                                
                                 <span class="progress-left">
                                     <span class="progress-bar"></span>
                                 </span>
                                 <span class="progress-right">
                                     <span class="progress-bar"></span>
                                 </span>
-                                <div class="progress-value">{{friendsTotalFb}} FB</div>
+                                <div class="progress-value">{{friendsTotalFb}}</div>
+                            </div>
+                        </div>
+                        <div class="col-md-3  center-block " v-tooltip.top-start="'Total de Classificação'">
+                            <div class="progress green">
+                                <span class="progress-left">
+                                    <span class="progress-bar"></span>
+                                </span>
+                                <span class="progress-right">
+                                    <span class="progress-bar"></span>
+                                </span>
+                                <div class="progress-value">{{progress()}}%</div>
                             </div>
                         </div>
                     </div>
@@ -95,7 +107,7 @@
                   </div>
                </div>
             </div>
-            <div class="alert alert-info alert-dismissible" role="alert">
+            <div v-show="contAllClassification != 0" class="alert alert-info alert-dismissible" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <strong>Você Classificou</strong> {{contAllClassification}} amigos(as) <span v-show="contNotClassification != 0">{{contNotClassification}} ainda espera por você!</span>
             </div>
@@ -308,13 +320,15 @@ export default {
                 }
         return litrs.length           
     }
+   
         
 	},	
 	methods: {
 		progress() {
-				//return (this.friendsTotalFb  * this.friendsTotalApp) / 100
-				return Math.round( this.contAllClassification * this.friendsTotalApp ) / 100
-		},
+        //return ( (this.friendsTotalApp - this.contAllClassification) / this.friendsTotalFb * 100)
+        //return Math.round( this.contAllClassification * this.friendsTotalApp ) / 100
+        return this.contAllClassification
+	},
 		scrollDireita() {
 				this.intervalo = setInterval(() => {
 						this.$refs.scroller.scrollLeft += 1
@@ -341,157 +355,11 @@ position: relative; border-radius: 86px; border: 2px solid #6961e2;}
 #menu-carrossel {margin-top: -40px;}
 
 
-.progressBarCirculos .centroDiv{margin-left: 10px}
-
-
-.progress{
-    width: 150px;
-    height: 150px;
-    line-height: 150px;
-    background: none;
-    margin: 0 auto;
-    box-shadow: none;
-    position: relative;
+.progressBarCirculos .centroDiv{margin-left: 12%;}
+@media (min-width : 320px) and (max-width : 480px){
+.progressBarCirculos .centroDiv {margin-left: 3%;}
 }
-.progress:after{
-    content: "";
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    border: 12px solid #fff;
-    position: absolute;
-    top: 0;
-    left: 0;
-}
-.progress > span{
-    width: 50%;
-    height: 100%;
-    overflow: hidden;
-    position: absolute;
-    top: 0;
-    z-index: 1;
-}
-.progress .progress-left{
-    left: 0;
-}
-.progress .progress-bar{
-    width: 100%;
-    height: 100%;
-    background: none;
-    border-width: 12px;
-    border-style: solid;
-    position: absolute;
-    top: 0;
-}
-.progress .progress-left .progress-bar{
-    left: 100%;
-    border-top-right-radius: 80px;
-    border-bottom-right-radius: 80px;
-    border-left: 0;
-    -webkit-transform-origin: center left;
-    transform-origin: center left;
-}
-.progress .progress-right{
-    right: 0;
-}
-.progress .progress-right .progress-bar{
-    left: -100%;
-    border-top-left-radius: 80px;
-    border-bottom-left-radius: 80px;
-    border-right: 0;
-    -webkit-transform-origin: center right;
-    transform-origin: center right;
-    animation: loading-1 1.8s linear forwards;
-}
-.progress .progress-value{
-    width: 90%;
-    height: 90%;
-    border-radius: 50%;
-    background: #44484b;
-    font-size: 24px;
-    color: #fff;
-    line-height: 135px;
-    text-align: center;
-    position: absolute;
-    top: 5%;
-    left: 5%;
-}
-.progress.blue .progress-bar{
-    border-color: #049dff;
-}
-.progress.blue .progress-left .progress-bar{
-    animation: loading-2 1.5s linear forwards 1.8s;
-}
-.progress.yellow .progress-bar{
-    border-color: #fdba04;
-}
-.progress.yellow .progress-left .progress-bar{
-    animation: loading-3 1s linear forwards 1.8s;
-}
-.progress.pink .progress-bar{
-    border-color: #ed687c;
-}
-.progress.pink .progress-left .progress-bar{
-    animation: loading-4 0.4s linear forwards 1.8s;
-}
-.progress.green .progress-bar{
-    border-color: #1abc9c;
-}
-.progress.green .progress-left .progress-bar{
-    animation: loading-5 1.2s linear forwards 1.8s;
-}
-@keyframes loading-1{
-    0%{
-        -webkit-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100%{
-        -webkit-transform: rotate(180deg);
-        transform: rotate(180deg);
-    }
-}
-@keyframes loading-2{
-    0%{
-        -webkit-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100%{
-        -webkit-transform: rotate(144deg);
-        transform: rotate(144deg);
-    }
-}
-@keyframes loading-3{
-    0%{
-        -webkit-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100%{
-        -webkit-transform: rotate(90deg);
-        transform: rotate(90deg);
-    }
-}
-@keyframes loading-4{
-    0%{
-        -webkit-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100%{
-        -webkit-transform: rotate(36deg);
-        transform: rotate(36deg);
-    }
-}
-@keyframes loading-5{
-    0%{
-        -webkit-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100%{
-        -webkit-transform: rotate(126deg);
-        transform: rotate(126deg);
-    }
-}
-@media only screen and (max-width: 990px){
-    .progress{ margin-bottom: 20px; }
-}
-
+/*Stilo progress bar  #44484b*/
+.progress{width:150px;height:150px;line-height:150px;background:0 0;margin:0 auto;box-shadow:none;position:relative}.progress .progress-bar,.progress:after,.progress>span{height:100%;position:absolute}.progress:after{content:"";width:100%;border-radius:50%;border:12px solid #fff;top:0;left:0}.progress>span{width:50%;overflow:hidden;top:0;z-index:1}.progress .progress-left{left:0}.progress .progress-bar{width:100%;background:0 0;border-width:12px;border-style:solid;top:0}.progress .progress-left .progress-bar{left:100%;border-top-right-radius:80px;border-bottom-right-radius:80px;border-left:0;-webkit-transform-origin:center left;transform-origin:center left}.progress .progress-right{right:0}.progress .progress-right .progress-bar{left:-100%;border-top-left-radius:80px;border-bottom-left-radius:80px;border-right:0;-webkit-transform-origin:center right;transform-origin:center right;animation:loading-1 1.8s linear forwards}.progress 
+.progress-value{width:90%;height:90%;border-radius:50%;background:#44484b;font-size:24px;color:#fff;line-height:135px;text-align:center;position:absolute;top:5%;left:5%}.progress.blue .progress-bar{border-color:#049dff}.progress.blue .progress-left .progress-bar{animation:loading-2 1.5s linear forwards 1.8s}.progress.yellow .progress-bar{border-color:#fdba04}.progress.yellow .progress-left .progress-bar{animation:loading-3 1s linear forwards 1.8s}.progress.pink .progress-bar{border-color:#ed687c}.progress.pink .progress-left .progress-bar{animation:loading-4 .4s linear forwards 1.8s}.progress.green .progress-bar{border-color:#1abc9c}.progress.green .progress-left .progress-bar{animation:loading-5 1.2s linear forwards 1.8s}@keyframes loading-1{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(180deg);transform:rotate(180deg)}}@keyframes loading-2{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(144deg);transform:rotate(144deg)}}@keyframes loading-3{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(90deg);transform:rotate(90deg)}}@keyframes loading-4{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(36deg);transform:rotate(36deg)}}@keyframes loading-5{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(126deg);transform:rotate(126deg)}}@media only screen and (max-width:990px){.progress{margin-bottom:20px}}
 </style>
